28c4dd6533fab897ab320fc9fff52855417d43c4
louiebasalo <louiebasalo.public@gmail.com>
2023-05-10 13:43:42 +0000

    vid# 116 Cart Icon & Dropdown and vid#117 Toggle Cart Open

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 import App from './App';
-import {UserProvider} from './contexts/user.context';
-import {ProductsProvider} from './contexts/product.context';
+import { UserProvider } from './contexts/user.context';
+import { ProductsProvider } from './contexts/product.context';
+import { CartProvider } from './contexts/cart.context';
 
 import './index.scss';
 
@@ -16,7 +17,9 @@ root.render(
     <BrowserRouter>
       <UserProvider>
         <ProductsProvider>
-          <App />
+          <CartProvider>
+            <App />
+          </CartProvider>
         </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
