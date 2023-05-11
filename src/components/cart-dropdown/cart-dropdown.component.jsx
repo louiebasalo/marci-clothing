6f2bd4783026d0439842eeff5cc3e4e6ee8e6985
louiebasalo <louiebasalo.public@gmail.com>
2023-05-11 15:00:52 +0000

    vid# 118 Add to Cart Pt. 1 a

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,13 @@
 import Button from '../button/button.component';
 import './cart-dropdown.styles.scss';
 
+import CartItem from '../cart-item/cart-item.component';
+
 const CartDropdown = () => {
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-                 
+                [].map(item => <CartItem cartItem = {item}/>)
             </div>
             <Button >GO TO CHECKOUT</Button>
         </div>
