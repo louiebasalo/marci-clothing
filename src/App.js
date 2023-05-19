bdef29eaca0d5ee67f3892815850f5e1c122bd91
louie <louiebasalo.public@gmail.com>
2023-05-19 23:04:39 +0800

    vid# 134. Nested Routes in Shop

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />}/>  {/*this will render at the outlet level */}
-        <Route path='shop' element={<Shop />}/>
+        <Route path='shop/*' element={<Shop />}/>
         <Route path='auth' element={<Authentication />}/>
         <Route path='checkout' element={<CheckOut />} />
 
