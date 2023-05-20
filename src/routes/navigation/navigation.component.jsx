bfcce450b55dfe986c6f01f2954fea756450a5ec
louie <louiebasalo.public@gmail.com>
2023-05-20 22:31:04 +0800

    vid# 137. Introducing Styled Components

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Link} from "react-router-dom";
+import { Outlet} from "react-router-dom";
 import {Fragment, useContext } from 'react'
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
@@ -10,7 +10,8 @@ import { CartContext } from "../../contexts/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import {ReactComponent as CrownLogo} from '../../assets/crown.svg'
-import './navigation.styles.scss'
+
+import {NavigationContainer, LogoContainer, NavLinks, NavLink} from './navigation.styles';
 
 const Navigation = () => {
 
@@ -20,26 +21,26 @@ const Navigation = () => {
 
     return (
       <Fragment>
-        <div className="navigation">
-            <Link className="logo-container" to="/">
+        <NavigationContainer>
+            <LogoContainer to="/">
                 <CrownLogo className="logo"/>
-            </Link>
-            <div className="nav-links-container">
-                 <Link className="nav-link" to='/shop'>
+            </LogoContainer>
+            <NavLinks>
+                 <NavLink to='/shop'>
                    SHOP
-                </Link>
+                </NavLink>
                 {currentUser ? (
-                  <span className="nav-link" onClick={signOutUser}>SIGN OUT</span> //video 108, stoped at minute 5:23
+                  <NavLink as='span' onClick={signOutUser}>SIGN OUT</NavLink> //video 108, stoped at minute 5:23
                 ):(
-                  <Link className="nav-link" to = '/auth'>
+                  <NavLink to = '/auth'>
                     SIGN IN
-                  </Link>
+                  </NavLink>
                 )}
                 <CartIcon />
                 
-            </div>
+            </NavLinks>
             {isCartOpen && <CartDropdown />}
-        </div>
+        </NavigationContainer>
         <Outlet /> 
       </Fragment>
       
