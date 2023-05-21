87ff6024ebd144b805887f4040cfcd674e142322
louie <louiebasalo.public@gmail.com>
2023-05-21 16:21:44 +0800

    activity: migrate checkout-item.styles from scss to styled component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -2,7 +2,7 @@
 import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
 
-import './checkout-item.styles.scss';
+import {CheckoutItemContainer, ImageContainer, BaseSpan,Quantity, Arrow, Value, RemoveButton} from './checkout-item.styles';
 
 const CheckoutItem = ({cartItem}) => {
     const { name, imageUrl, price, quantity} = cartItem;
@@ -14,23 +14,27 @@ const CheckoutItem = ({cartItem}) => {
 
 
     return (
-        <div className='checkout-item-container'>
-            <div className='image-container'>
+        <CheckoutItemContainer>
+            <ImageContainer>
                 <img src={imageUrl} alt={`${name}`} />
-            </div>
-            <span className='name'> {name} </span>
-            <span className='quantity'> 
-                <div className='arrow' onClick={removeItemHandler}>
+            </ImageContainer>
+            <BaseSpan> {name} </BaseSpan>
+            <Quantity> 
+                <Arrow onClick={removeItemHandler}>
                     &#10094;
-                </div>
-                    <span className='value'>{quantity}</span>
-                <div className='arrow' onClick={addItemHandler}>
+                </Arrow>
+                    <Value>{quantity}</Value>
+                <Arrow className='arrow' onClick={addItemHandler}>
                     &#10095;
-                </div>
-            </span>
-            <span className='price'> {price} </span>
-            <div className='remove-button'><span onClick={clearItemHandler}>&#10006;</span></div>
-        </div>
+                </Arrow>
+            </Quantity>
+            <BaseSpan> {price} </BaseSpan>
+            <RemoveButton>
+                <BaseSpan onClick={clearItemHandler}>
+                    &#10006;
+                </BaseSpan>
+            </RemoveButton>
+        </CheckoutItemContainer>
     )
 
 }
