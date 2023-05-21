b4bdff407ce5a2c6b2ef84ddb74b5de2860015e0
louie <louiebasalo.public@gmail.com>
2023-05-21 21:53:13 +0800

    vid# 141. Styled-component Form-input Component

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -4,9 +4,7 @@ const DirectoryItem = ({category}) => {
     const {imageUrl, title} = category;
     return (
         <DirectoryItemContainer>
-        <BackgroundImage style={{
-          backgroundImage: `url(${imageUrl})`
-        }}/>
+        <BackgroundImage imageUrl={imageUrl}/>
         <Body>
           <h2>{title}</h2>
           <p>Shop Now</p>
