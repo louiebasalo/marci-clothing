480284779d5b08a4079307402a47f7f887d982cb
louie <louiebasalo.public@gmail.com>
2023-05-21 21:31:29 +0800

    vid# 140. Styled-component Directroy-item and Cart Icon

--- src/components/directory-item/directory-item.styles.jsx
+++ src/components/directory-item/directory-item.styles.jsx
@@ -1,37 +1,12 @@
 
 import styled from 'styled-components';
 
-export const DirectoryItemContainer = styled.div`
-  min-width: 30%;
-  height: 240px;
-  flex: 1 1 auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: 1px solid black;
-  margin: 0 7.5px 15px;
-  overflow: hidden;
-  
-  &:hover {
-    cursor: pointer;
-  
-    & .background-image {
-      transform: scale(1.1);
-      transition: transform 3s cubic-bezier(0.25, 0.45, 0.45, 0.95);
-    }
-  
-    & .body {
-      opacity: 0.9;
-    }
-  }
-
-`;
-
 export const BackgroundImage = styled.div`
   width: 100%;
   height: 100%;
   background-size: cover;
   background-position: center;
+  background-image: ${({imageUrl}) => `url(${imageUrl})`};
 
 `;
 
@@ -52,6 +27,7 @@ export const Body = styled.div`
     margin: 0 6px 0;
     font-size: 22px;
     color: #4a4a4a;
+    text-transform: uppercase;
   }
   
   p {
@@ -60,6 +36,42 @@ export const Body = styled.div`
   }
 
 `;
+
+export const DirectoryItemContainer = styled.div`
+  min-width: 30%;
+  height: 240px;
+  flex: 1 1 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  margin: 0 7.5px 15px;
+  overflow: hidden;
+  
+  &:hover {
+    cursor: pointer;
+  
+    & ${BackgroundImage} {
+      transform: scale(1.1);
+      transition: transform 3s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+    }
+  
+    & ${Body} {
+      opacity: 0.9;
+    }
+  }
+
+  &:first-child {
+    margin-right: 7.5px;
+  }
+      
+  &:last-child {
+    margin-left: 7.5px;
+  }
+
+`;
+
+
 
 // .directory-item-container {
 //     min-width: 30%;
