c0ffbc95720652fe46d0dcb363042c076e9502af
louiebasalo <louiebasalo.public@gmail.com>
2023-06-16 08:59:57 +0000

    rearranged Category Context

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import reportWebVitals from './reportWebVitals';
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
-import { CategoriesProvider } from './contexts/categories.context';
+// import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 
 import './index.scss';
@@ -16,11 +16,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,16 +2,20 @@ import { Routes, Route } from 'react-router-dom';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
+import { CategoriesProvider } from '../../contexts/categories.context';
 
 import './shop.styles.scss';
 
 
 const Shop = () => {
     return (
-        <Routes>
-            <Route index element= {<CategoriesPreview />} />
-            <Route path=':category' element={<Category />} />
-        </Routes>
+        <CategoriesProvider>
+            <Routes>
+                <Route index element= {<CategoriesPreview />} />
+                <Route path=':category' element={<Category />} />
+            </Routes>
+        </CategoriesProvider>
+
     )
 }
 
