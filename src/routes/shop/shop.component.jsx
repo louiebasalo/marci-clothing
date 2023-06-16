99a4246d82aed2f3ecf51329adb2be38d9fd3b0e
louiebasalo <louiebasalo.public@gmail.com>
2023-06-16 09:00:53 +0000

    rearranged Category Context bacj

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,20 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
-import { CategoriesProvider } from '../../contexts/categories.context';
 
 import './shop.styles.scss';
 
 
 const Shop = () => {
     return (
-        <CategoriesProvider>
-            <Routes>
-                <Route index element= {<CategoriesPreview />} />
-                <Route path=':category' element={<Category />} />
-            </Routes>
-        </CategoriesProvider>
-
+        <Routes>
+            <Route index element= {<CategoriesPreview />} />
+            <Route path=':category' element={<Category />} />
+        </Routes>
     )
 }
 
