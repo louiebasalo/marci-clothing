e3c1cfce837ed1ec0b4116e9a35bb1c299c0616d
louie <louiebasalo.public@gmail.com>
2023-06-19 21:31:15 +0800

    vid# 155. React-Redux: Creating User Reducer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import {Routes,Route} from 'react-router-dom';
 
+import { useEffect } from "react";
+import { useDispatch } from 'react-redux';
+import { onAuthStateChangedListener, createUserDocumentFromAuth} from "./utils/firebase/firebase.utils";
+
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
-
 import CheckOut from './routes/checkout/checkout.component';
+import {setCurrentUser} from './store/user/user.action';
 
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+        if (user){
+            createUserDocumentFromAuth(user);
+        }
+        dispatch(setCurrentUser(user));
+    });
+    return unsubscribe;
+  }, []);
+
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 
 import App from './App';
-import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 import { store } from './store/store';
@@ -18,13 +17,11 @@ root.render(
   <React.StrictMode>
     <Provider store={store}> 
       <BrowserRouter>
-        <UserProvider>
           <CategoriesProvider>
             <CartProvider>
               <App />
             </CartProvider>
           </CategoriesProvider>
-        </UserProvider>
       </BrowserRouter>
     </Provider>
     
