4883055c41945aec3b81b515d0ea22b2a6e28ccb
louiebasalo <louiebasalo.public@gmail.com>
2023-08-19 08:26:31 +0000

    vid#56. React-Redux: Selector

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,14 @@
 import { Outlet} from "react-router-dom";
 import {Fragment, useContext } from 'react'
 
+import { useSelector } from "react-redux";
+
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
-import { UserContext } from "../../contexts/user.context";
 import { CartContext } from "../../contexts/cart.context";
+
+import { selectCurrentUser } from "../../store/user/user.selector";
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
@@ -15,7 +18,7 @@ import {NavigationContainer, LogoContainer, NavLinks, NavLink} from './navigatio
 
 const Navigation = () => {
 
-  const {currentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser); 
   const {isCartOpen} = useContext(CartContext);
   
 
---
+++ src/store/user/user.selector.js
@@ -0,0 +1,2 @@
+
+export const selectCurrentUser = (state) => state.user.currentUser; //this is how you select values off of redux and then put then into components.
