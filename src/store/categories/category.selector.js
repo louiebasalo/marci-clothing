97e69fea826bbe431871b8856e7bbb1763bb9628
louiebasalo <louiebasalo.public@gmail.com>
2023-08-21 08:57:03 +0000

    vid#164, making catgories selector a memoized selector

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,9 +1,20 @@
 
-export const selectCategoriesMap = (state) => {
-    console.log('selector fired');
-    return state.categories.categories.reduce((acc, category) => {
+import { createSelector } from "reselect";
+
+const selectCategoriesReducer = (state) => 
+    state.categories; //the entire sate as parameters, and returing only the categories slice
+
+export const selectCategories = createSelector(
+    [selectCategoriesReducer],  //first is an array of input selectors
+    (categoriesSlice) =>  categoriesSlice.categories  //second is the output selector
+);  //memoize selector and it takes 2 arguments
+
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    (categories) =>  
+        categories.reduce((acc, category) => {
         const {title, items} = category;
         acc[title.toLowerCase()] = items;
         return acc;
-    }, {});
-}
+    }, {}) 
+)
