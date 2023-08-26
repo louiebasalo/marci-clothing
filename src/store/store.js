438cd3f72e85c3f8fde2a9b47c953a9d5fd804cd
louiebasalo <louiebasalo.public@gmail.com>
2023-08-26 16:36:47 +0000

    vid# 174. Asynchronous redux: Redux-Saga

--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,10 @@ import {compose, legacy_createStore as createStore, applyMiddleware} from 'redux
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+
+import { rootSaga } from './root-saga';
 
 import { rootReducer } from './root-reducer';
 
@@ -15,15 +18,19 @@ const persistConfig = {
     blacklist: ['cart']
 }
 
+const sagaMiddleware = createSagaMiddleware();
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean);
+const middleWares = [process.env.NODE_ENV !== 'production' && logger, sagaMiddleware].filter(Boolean);
 
 const composedEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers);  //generate the store
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
