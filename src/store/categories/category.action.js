293cdd55396f6e482fe2dce2fadcc45a05056995
louiebasalo <louiebasalo.public@gmail.com>
2023-08-26 05:15:43 +0000

    vid# 172. Redux-Thunk Pt.2

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,5 +1,31 @@
 import { createAction } from '../../utils/reducer/reducer.utils';
 
 import { CATEGORIES_ACTION_TYPES } from './category.types';
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 
 export const setCategories = (categoriesArray) => createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
+
+export const fetchCategoriesStart = () => createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (categoriesArray) => 
+    createAction(
+        CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+        categoriesArray    
+    );
+
+export const fetchCategoriesFailed = (error) => 
+    createAction(
+        CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED,
+        error
+    );
+
+//this is the thunk, an asynchronous function // it's just a function that returns a function that gets a dispatch
+export const fetchCategoriesAsync = () => async (dispatch) => {
+    dispatch(fetchCategoriesStart());
+    try {
+        const categoriesArray = await getCategoriesAndDocuments('categories');
+        dispatch(fetchCategoriesSuccess(categoriesArray));
+    } catch (error) {
+        dispatch(fetchCategoriesFailed(error));
+    }
+};
