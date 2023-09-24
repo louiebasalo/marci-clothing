deb9a0aa3880181b602d6289085e5a5798031ab4
louie <louiebasalo.public@gmail.com>
2023-09-24 15:47:17 +0800

    vid #195. Finishing Stripe Payment

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -5,11 +5,8 @@ import Button, {BUTTON_TYPE_CLASSES} from '../button/button.component';
 import { PaymentFormContainer, FormContainer } from "./payment-form.styles";
 
 const PaymentForm = () => {
-
-    const PaymentForm = () => {
-        const stripe = useStripe();
-        const elements = useElements();
-    }
+    const stripe = useStripe();
+    const elements = useElements();
 
     const paymentHandler = async (e) => {
         e.preventDefault();
@@ -18,12 +15,41 @@ const PaymentForm = () => {
             return;
         }
 
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ amount: 10000})
+        }).then(res => res.json());
+
+        const {
+            paymentIntent: { client_secret},
+        } = response;
+        console.log(client_secret);
+
+        const paymentResult = await stripe.confirmCardPayment(client_secret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: 'louie'
+                }
+            }
+        });
+
+        if(paymentResult.error){
+            alert(paymentResult.error);
+        }else{
+            if(paymentResult.paymentIntent.status == 'succeeded'){
+                alert('payment successful');
+            }
+        }
         
     }
 
     return (
         <PaymentFormContainer>
-            <FormContainer>
+            <FormContainer onSubmit={paymentHandler}>
                 <h2>Credit Card payment</h2>
                 <CardElement/>
                 <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
